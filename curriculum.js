2b016f2eb1823d32b2169e269da93d90d655c64c
mayanjun <mayanjun@flashexpress.com>
2026-05-07 11:09:01 +0800

    feat: 增加 90 天自动化启蒙学习计划与词库系统

---
+++ curriculum.js
@@ -0,0 +1,82 @@
+// 3个月英语启蒙词库（带权威B站资源）
+const curriculumLibrary = [
+  {
+    scene: "早上见面",
+    icon: "☀️",
+    desc: "和家人打招呼，开启美好的一天",
+    dialogs: [
+      { english: "Good morning!", chinese: "早上好！", phonetic: "/ɡʊd ˈmɔːrnɪŋ/" },
+      { english: "Wake up, baby!", chinese: "起床啦，宝宝！", phonetic: "/weɪk ʌp ˈbeɪbi/" }
+    ],
+    tip: "1. 拉开窗帘\n2. 摸摸头\n3. 拥抱",
+    resources: [
+      { type: "song", name: "清华幼儿英语：Good Morning", url: "https://www.bilibili.com/video/BV15y4y1p7zP" },
+      { type: "song", name: "Super Simple Songs: Good Morning", url: "https://www.bilibili.com/video/BV154411u7zS" }
+    ]
+  },
+  {
+    scene: "吃饭",
+    icon: "🍚",
+    desc: "和家人一起享用美味的饭菜",
+    dialogs: [
+      { english: "Let's eat!", chinese: "我们吃饭吧！", phonetic: "/lets iːt/" },
+      { english: "Is it yummy?", chinese: "好吃吗？", phonetic: "/ɪz ɪt ˈjʌmi/" }
+    ],
+    tip: "1. 坐好\n2. 自己拿勺子\n3. 大口吃",
+    resources: [
+      { type: "song", name: "SSS: Yummy Yummy", url: "https://www.bilibili.com/video/BV154411u7zS" }
+    ]
+  },
+  {
+    scene: "喝水",
+    icon: "💧",
+    desc: "多喝水，身体好",
+    dialogs: [
+      { english: "Time for water!", chinese: "喝水时间到！", phonetic: "/taɪm fɔːr ˈwɔːtər/" },
+      { english: "Drink some water.", chinese: "喝点水吧。", phonetic: "/drɪŋk sʌm ˈwɔːtər/" }
+    ],
+    tip: "1. 拿着小杯子\n2. 慢慢喝\n3. 夸奖宝宝",
+    resources: [
+      { type: "song", name: "Wow English: Drink Water", url: "https://www.bilibili.com/video/BV17t411q7p6" }
+    ]
+  },
+  {
+    scene: "上厕所",
+    icon: "🚽",
+    desc: "养成良好的卫生习惯",
+    dialogs: [
+      { english: "Need to potty?", chinese: "要尿尿吗？", phonetic: "/niːd tuː ˈpɒti/" },
+      { english: "Let's go to the potty.", chinese: "我们去上厕所吧。", phonetic: "/lets ɡoʊ tuː ðə ˈpɒti/" }
+    ],
+    tip: "1. 发现宝宝憋尿\n2. 及时询问\n3. 鼓励自主",
+    resources: [
+      { type: "video", name: "BBC: Potty Training", url: "https://www.bilibili.com/video/BV1wJ411D7Sj" }
+    ]
+  },
+  {
+    scene: "洗澡",
+    icon: "🛁",
+    desc: "洗得香香的",
+    dialogs: [
+      { english: "Time for a bath!", chinese: "洗澡时间到！", phonetic: "/taɪm fɔːr ə bæθ/" },
+      { english: "It's warm!", chinese: "水很暖和！", phonetic: "/ɪts wɔːrm/" }
+    ],
+    tip: "1. 准备好浴巾\n2. 玩水玩具\n3. 快乐洗澡",
+    resources: [
+      { type: "song", name: "SSS: Bath Time", url: "https://www.bilibili.com/video/BV154411u7zS" }
+    ]
+  },
+  {
+    scene: "晚安",
+    icon: "🌙",
+    desc: "温柔的晚安，做个甜甜的梦",
+    dialogs: [
+      { english: "Good night!", chinese: "晚安！", phonetic: "/ɡʊd naɪt/" },
+      { english: "Sweet dreams!", chinese: "做个好梦！", phonetic: "/swiːt driːmz/" }
+    ],
+    tip: "1. 关上灯\n2. 盖好被子\n3. 轻轻拍背",
+    resources: [
+      { type: "song", name: "Twinkle Twinkle Little Star", url: "https://www.bilibili.com/video/BV1xJ411W79c" }
+    ]
+  }
+];
